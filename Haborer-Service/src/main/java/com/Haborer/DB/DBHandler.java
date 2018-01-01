a41b12aa40b4faf7c71fa6bcab61f7354bbb9005
אחימאיר שנולד <achimeir.shenwald@gmail.com>
2018-01-01 23:10:27 +0200

    The handler is working and been tested

--- Haborer-Service/src/main/java/com/Haborer/DB/DBHandler.java
+++ Haborer-Service/src/main/java/com/Haborer/DB/DBHandler.java
@@ -1,5 +1,5 @@
 package com.Haborer.db;
-import db.Utilities;
+import com.Haborer.db.Utilities;
 import java.rmi.UnknownHostException;
 import java.util.List;
 import java.util.Set;
@@ -9,12 +9,9 @@ import com.mongodb.*;
 
 public class DBHandler {
 
-// private static MongoClientURI connectionString = null;
-// private static MongoClient mongoClient = null;
-//private static MongoDatabase mdb = null;;
 private static MongoURI connectionString = null;
 private static Mongo mongoClient = null;
-private static DB mdb = null
+private static DB mdb = null;
 
 public DBHandler() {
     if (mdb == null) {
@@ -24,7 +21,7 @@ public DBHandler() {
     }
 }
 
-public void insertObject(DBObject obj, String CollectionName) {
+public void insertObj(DBObject obj, String CollectionName) {
     try{
          DBCollection collection = mdb.getCollection(CollectionName);
          collection.insert(obj);   
@@ -34,28 +31,24 @@ public void insertObject(DBObject obj, String CollectionName) {
     }
 }
 
-// public void updateObject(Document oldObj ,Document newObj, String CollectionName) {
-//     try{   
-//         mdb.getCollection(CollectionName).updateOne(oldObj,{$set:newObj});
-//        }
-//    catch (Exception e) {
-//        e.printStackTrace();
-//    }
-// }
-    //     collection=mdb.getCollection(CollectionName);;tionName);;
-    //     collection=mdb.getCollection(CollectionName);    updateObject.put("$set", newObj);
-    //     collection.update(oldObj, updateObject);
-    // }
-    // catch (Exception e) {
-    //     e.printStackTrace();
-    // }
+public void updateObj(DBObject oldObj ,DBObject newObj, String CollectionName) {
+    try{   
+         DBCollection collection = mdb.getCollection(CollectionName);
+         collection.update(oldObj,newObj);
+       }
+   catch (Exception e) {
+       e.printStackTrace();
+   }
+}
 
-// public void removeObj(Document obj, String CollectionName) {
-//     try{   
-//         mdb.getCollection(CollectionName).removeOne(obj);
-//        }
-//    catch (Exception e) {
-//        e.printStackTrace();
-//    }
-//     }
+
+public void removeObj(DBObject obj, String CollectionName) {
+    try{   
+        DBCollection collection = mdb.getCollection(CollectionName);
+        collection.remove(obj);
+       }
+   catch (Exception e) {
+       e.printStackTrace();
+   }
+    }
 }
--- Haborer-Service/src/main/java/com/Haborer/db/dbtest.java
+++ Haborer-Service/src/main/java/com/Haborer/db/dbtest.java
@@ -1,34 +1,29 @@
 package main.java.com.Haborer.db;
 import java.util.Map;
 import java.util.Set;
-
 import org.bson.BSONObject;
 import org.bson.Document;
-
-import com.mongodb.*;
 import com.Haborer.db.DBHandler;
-import com.mongodb.DB;
-import com.mongodb.DBObject;
-import com.mongodb.DBObjectCodec;
-import com.mongodb.Mongo;
+import com.mongodb.*;
 
 public class dbtest {
     public static void main(String[] args) {
-
-        // BasicDBObject nor= new BasicDBObject();
-        // nor.put("name", "achi");
-        // nor.put("age", 25);
-        // nor.put(("name"),"johan");
-        // Document doc= new Document("pop", 34);
-        // BasicDBObject doca = new BasicDBObject(doc);
-        // Document doc2= new Document("pop", 35);
         BasicDBObject document = new BasicDBObject();
-	    document.put("database", "mkyongDB");
-	    document.put("table", "hosting");
+        BasicDBObject documento = new BasicDBObject();
+        document.put("pop", 34);
+        documento.put("pop", 35);
+        documento.put("name", "avi");
         DBHandler handler = new DBHandler();
-        handler.insertObject(document, "Men");
-//        handler.updateObject(doc,doc2, "Men");
-       // handler.removeObj(doc, "Men");
-        System.out.print(5555);
+        
+        /*Test Insert Object: */ 
+        handler.insertObj(document, "Men");
+        
+        /*Test Update Object: */
+        handler.updateObj(document, documento, "Men");
+
+        /*Test Remove Object: */
+        handler.removeObj(document, "Men");
+
+        System.out.print("Finish Test1...");
     }
 }
